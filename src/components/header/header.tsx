b5cc3c714e635ecee3421db9fdbe715c1d7ebe24
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2022-12-30 22:52:11 +0300

    Updates

--- src/components/header/header.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-export const Header: FC = () => {
-  return (
-    <header className="container border-bottom p-3 mb-5">
-      <h1 className="text-white text-center">Чат для одиноких людей</h1>
-    </header>
-  );
-}
